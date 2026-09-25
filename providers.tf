69d4075db196010b003543011380ffe5799e745a
Manju Sam <manjusam403@gmail.com>
2026-09-26 01:56:06 +1000

    Configure S3 remote Terraform backend

--- providers.tf
+++ providers.tf
@@ -10,6 +10,15 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "manjusam-terraform-state-838291"
+    key          = "aws-cloud-operations-poc/terraform.tfstate"
+    region       = "ap-southeast-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+
 }
 
 provider "aws" {
